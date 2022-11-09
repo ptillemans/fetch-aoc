89392b4ad87c8ebd11a15482172f0be6b7e652bf
ptillemans <pti@snamellit.com>
2022-11-09 21:31:51 +0100

    create compiling cargo project

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, collections::BTreeMap};
 use std::sync::Arc;
 use std::fs;
 
@@ -6,6 +6,7 @@ use anyhow::{Context, Result};
 use chrono::Datelike;
 use clap::Parser;
 use reqwest::{cookie::Jar, Url};
+use handlebars::Handlebars;
 
 #[derive(Parser)]
 #[command(author, version, long_about = None)]
@@ -22,7 +23,6 @@ struct Config {
     year: u16,
     day: u16,
     session: String,
-    data_folder: PathBuf,
 }
 
 impl Config {
@@ -32,8 +32,7 @@ impl Config {
         let year = cli.year.unwrap_or(now.year() as u16);
         let day = cli.day.unwrap_or(now.day() as u16);
         let session = cli.session;
-        let data_folder = PathBuf::from("data");
-        Config { year, day, session, data_folder }
+        Config { year, day, session }
     }
 }
 
@@ -59,25 +58,70 @@ async fn fetch_page(cfg: &Config, page: &str) -> Result<String> {
         .context("getting page from aoc site")
 }
 
-fn create_data_folder(cfg: &Config) -> Result<()> {
-    fs::create_dir_all(&cfg.data_folder.as_path()).context("Create data folder")
+fn write_file(cfg: &Config, name: &PathBuf, contents: &str) -> Result<()> {
+    let base = PathBuf::from(format!("aoc-{}-day-{}", cfg.year, cfg.day));
+    let fullname = base.join(name);
+    fs::create_dir_all(fullname.parent().unwrap())?;
+    if !fullname.exists() {
+        fs::write(&fullname, contents).context(format!("Writing {:?}", &fullname))
+    } else {
+        println!("File {:?} already exists.", &fullname);
+        Ok(())
+    }
 }
 
 async fn fetch_challenge(cfg: &Config) -> Result<()> {
     let path = format!("{}/day/{}", cfg.year, cfg.day);
     let challenge = fetch_page(cfg, &path).await?;
     let markdown = html2md::parse_html(&challenge);
-    let filename = cfg.data_folder.join("challenge.md");
-    fs::write(&filename, &markdown)?;
+    let data_folder = PathBuf::from("data");
+    fs::create_dir_all(data_folder.as_path()).context("Create data folder")?;
+    let filename = data_folder.join("challenge.md");
+    write_file(&cfg, &filename,  &markdown)?;
     Ok(())
 }
 
 async fn fetch_input(cfg: &Config) -> Result<()> {
     let path = format!("{}/day/{}/input", cfg.year, cfg.day);
     let input = fetch_page(cfg, &path).await?;
-    let filename = cfg.data_folder.join("input.txt");
-    fs::write(&filename, &input)?;
+    let filename = PathBuf::from("data/input.txt");
+    write_file(&cfg, &filename, &input)?;
     Ok(())
+}
+
+const MAIN_TEMPLATE: &str = include_str!("../template/main.rs");
+const CARGO_TEMPLATE: &str = include_str!("../template/Cargo.toml");
+
+fn create_template_engine(_cfg: &Config) -> Result<Handlebars> {
+    let mut handlebars = Handlebars::new();
+    handlebars.register_template_string("source_code", MAIN_TEMPLATE)?;
+    handlebars.register_template_string("cargo_toml", CARGO_TEMPLATE)?;
+    Ok(handlebars)
+}
+
+fn create_template_data(cfg: &Config) -> BTreeMap<String, String> {
+    let mut data: BTreeMap<String, String> = BTreeMap::new();
+    data.insert("year".to_string(), cfg.year.to_string());
+    data.insert("day".to_string(), cfg.day.to_string());
+    data
+}
+
+fn generate_main_rs(cfg: &Config) -> Result<()> {
+    let src_folder = PathBuf::from("src");
+    fs::create_dir_all(src_folder.as_path()).context("Create src folder")?;
+    let filename = src_folder.join("main.rs");
+    let engine = create_template_engine(cfg)?;
+    let data = create_template_data(cfg);
+    let source_code = engine.render("source_code", &data).context("Rendering source code")?;
+    write_file(&cfg, &filename, &source_code).context("Writing source code")
+}
+
+fn generate_cargo_toml(cfg: &Config) -> Result<()> {
+    let filename = PathBuf::from("Cargo.toml");
+    let engine = create_template_engine(cfg)?;
+    let data = create_template_data(cfg);
+    let source_code = engine.render("cargo_toml", &data).context("Rendering Cargo.toml")?;
+    write_file(&cfg, &filename, &source_code).context("Writing Cargo.toml")
 }
 
 #[tokio::main]
@@ -87,9 +131,9 @@ async fn main() -> Result<()> {
     println!("year: {}", cfg.year);
     println!("day: {}", cfg.day);
     println!("session: {}", cfg.session);
-    create_data_folder(&cfg)?;
     fetch_challenge(&cfg).await?;
     fetch_input(&cfg).await?; 
-
+    generate_main_rs(&cfg)?;
+    generate_cargo_toml(&cfg)?;
     Ok(())
 }
